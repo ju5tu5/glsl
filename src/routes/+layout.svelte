120ccd9a9486a31a0cac629ba9e74824668b9127
Justus Sturkenboom <justus@ju5tu5.nl>
2023-12-24 09:17:57 +0100

    Sveltekit installation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<header>
+  <h1>GLSL</h1>
+</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p><span>&copy;</span> Copyleft, all wrongs reversed.</p>
+</footer>
+
+<style>
+  span {
+    display: inline-block;
+    transform: rotate(180deg);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h2>Welcome to SvelteKit</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
