b5ad7a986bb17108df1eecbc422343055d1ba94a
Justus Sturkenboom <justus@ju5tu5.nl>
2023-12-24 09:43:47 +0100

    changed config file

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,12 @@ const config = {
     adapter: adapter({
       fallback: '404.html',
     }),
-  },
-  paths: {
-    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    },
   },
 }
+
+console.log('Building with base:' + config.kit.paths.base)
 
 export default config
